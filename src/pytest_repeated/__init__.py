54acf6e6b1a3b25ba95fe941e43b059a21d3a3d3
Sinan Ozel <4183117+sinan-ozel@users.noreply.github.com>
2025-12-11 05:50:56 +0000

    Fix the pipeline to deploy the github pages

--- src/pytest_repeated/__init__.py
+++ src/pytest_repeated/__init__.py
@@ -1,7 +1,7 @@
 """Pytest-repeated plugin for running tests multiple times with
 threshold-based passing."""
 
-__version__ = "0.3.6"
+__version__ = "0.3.7"
 
 from .plugin import (
     pytest_configure,
